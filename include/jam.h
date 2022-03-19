e3ab2cc2e845e7cf0c85595575b4cad64b3b2204
Cogal <mathieu.rio@epitech.eu>
2022-03-19 12:48:31 +0100

    feat: song bar

--- include/jam.h
+++ include/jam.h
@@ -14,7 +14,8 @@
 
     #include <SFML/Graphics.h>
     #include <SFML/System.h>
-
+    
+    #include <stdio.h>
     #include <stdlib.h>
     #include <stdbool.h>
     #include <unistd.h>
--- src/settings_menu/settings.c
+++ src/settings_menu/settings.c
@@ -31,7 +31,7 @@ void define_front_img(all *opti)
 {
     int x = 150;
     int y = 490;
-    sfVector2f size = {1 ,0.6};
+    sfVector2f size = {0.1 ,0.6};
     sfVector2f pos = {x, y};
     opti->settings.img[1].sprite = sfSprite_create();
     sfSprite_setPosition(opti->settings.img[1].sprite, pos);
@@ -40,6 +40,31 @@ void define_front_img(all *opti)
     sfSprite_setTexture(opti->settings.img[1].sprite, opti->settings.img[1].texture, sfTrue);
 }
 
+void button_song(sfVector2i mouse_pos, all *opti)
+{
+    if (mouse_pos.y >= 476 && mouse_pos.y <= 526) {
+        if (mouse_pos.x >= 152 && mouse_pos.x <= 650) {
+            while (sfRenderWindow_pollEvent(opti->settings.window.window, &opti->settings.window.event)) {
+                if (opti->settings.window.event.type == sfEvtMouseButtonPressed) {
+                    printf("song\n");
+                    int x = mouse_pos.x;
+                    int y = 490;
+                    sfVector2f pos = {x, y};
+                    sfSprite_setPosition(opti->settings.img[1].sprite, pos);
+                    break;
+                }
+            }
+        }
+    }
+} 
+
+void check_pos(all *opti)
+{
+    sfVector2i mouse_pos = sfMouse_getPosition((const sfWindow *)opti->settings.window.window);
+    button_song(mouse_pos, opti);
+}
+
+// initialise les images
 void init_img(all *opti)
 {
     opti->settings.img = malloc(sizeof(opti->settings.img) * 10);
@@ -64,6 +89,8 @@ void settings_menu(void)
 
         sfRenderWindow_drawSprite(opti.settings.window.window, opti.settings.img[1].sprite, NULL);
         sfRenderWindow_drawSprite(opti.settings.window.window, opti.settings.img[0].sprite, NULL);
+
+        check_pos(&opti);
 
         sfRenderWindow_display(opti.settings.window.window);
     }
