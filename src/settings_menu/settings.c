ec477834440ce731d1f65923a57a0efc41899a0c
Cogal <mathieu.rio@epitech.eu>
2022-03-19 12:13:16 +0100

    feat: add template son

--- src/settings_menu/settings.c
+++ src/settings_menu/settings.c
@@ -16,15 +16,35 @@ void init_window(int w, int h, int other, all *opti)
 
 void define_back_img(all *opti)
 {
+    int x = 150;
+    int y = 450;
+    sfVector2f size = {1 ,2};
+    sfVector2f pos = {x, y};
     opti->settings.img[0].sprite = sfSprite_create();
+    sfSprite_setPosition(opti->settings.img[0].sprite, pos);
+    sfSprite_setScale(opti->settings.img[0].sprite, size);
     opti->settings.img[0].texture = sfTexture_createFromFile("./content/back_life.png", NULL);
     sfSprite_setTexture(opti->settings.img[0].sprite, opti->settings.img[0].texture, sfTrue);
+}
+
+void define_front_img(all *opti)
+{
+    int x = 150;
+    int y = 490;
+    sfVector2f size = {1 ,0.6};
+    sfVector2f pos = {x, y};
+    opti->settings.img[1].sprite = sfSprite_create();
+    sfSprite_setPosition(opti->settings.img[1].sprite, pos);
+    sfSprite_setScale(opti->settings.img[1].sprite, size);
+    opti->settings.img[1].texture = sfTexture_createFromFile("./content/front_life.png", NULL);
+    sfSprite_setTexture(opti->settings.img[1].sprite, opti->settings.img[1].texture, sfTrue);
 }
 
 void init_img(all *opti)
 {
     opti->settings.img = malloc(sizeof(opti->settings.img) * 10);
     define_back_img(opti);
+    define_front_img(opti);
 }
 
 // settings main_menu -> settings or Pause -> settings or reverse
@@ -42,6 +62,7 @@ void settings_menu(void)
 
         sfRenderWindow_clear(opti.settings.window.window, sfWhite);
 
+        sfRenderWindow_drawSprite(opti.settings.window.window, opti.settings.img[1].sprite, NULL);
         sfRenderWindow_drawSprite(opti.settings.window.window, opti.settings.img[0].sprite, NULL);
 
         sfRenderWindow_display(opti.settings.window.window);
